73682efc94760facad53a603387950a5ce8563ed
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2022-01-17 14:24:13 +0100

    rotation des chevrons de la page: a propos

--- mon-app/src/components/Block_collapse.js
+++ mon-app/src/components/Block_collapse.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react/cjs/react.development";
 import "../styles/Block_collapse.css";
+import Vector from "../assets/Vector.png"
 
 const BlockCollapse = (props) => {
   const [isCollapsed, setIsCollapsed] = useState("collapsed");
@@ -11,8 +12,8 @@ const BlockCollapse = (props) => {
         className="collapse-button"
         onClick={() => setIsCollapsed(!isCollapsed)}
       >
-        {" "}
         <h3>{title}</h3>
+        <i className={`fas fa-chevron-down ${isCollapsed ? "" : "rotate"}`}></i>
       </button>
       <div
         className={`collapse-content ${isCollapsed ? "collapsed" : "expanded"}`}
