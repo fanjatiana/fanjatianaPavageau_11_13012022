9e4fd107a8f2dda6bc08b1053467b35db75ec8e8
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2022-01-22 14:50:22 +0100

    responsive page erreur 404

--- mon-app/src/pages/NotFound.js
+++ mon-app/src/pages/NotFound.js
@@ -4,7 +4,7 @@ import "../styles/NotFound.css";
 const NotFound = () => {
   return (
     <main className="notFound_container">
-      <div className="Error-message">
+      <div className="error-message">
         <h2>404</h2>
         <p>Oups! La page que vous demandez n'existe pas.</p>
       </div>
