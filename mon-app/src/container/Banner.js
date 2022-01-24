9798208f7f9aa1a1483918079f78ff8198a475b0
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2022-01-24 16:14:15 +0100

    modifications de style css + responsive

--- mon-app/src/container/Banner.js
+++ mon-app/src/container/Banner.js
@@ -7,14 +7,7 @@ function Banner(props) {
     <div className="banner">
       <div className="bg_filter bg_img" 
       style={{  
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),url(${imgSrc})`,
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        display:"flex",
-        justifyContent:"center",
-        alignItems: "center",
-        borderRadius:"25px"
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),url(${imgSrc})`
       }}
         >
         <h2>{title}</h2>
